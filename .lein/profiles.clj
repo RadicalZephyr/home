c9bb53e6d7776a14af0a49ebf812694f1d039bc7
Geoff Shannon <earthlingzephyr@gmail.com>
2013-02-01 16:45:25 -0800

    Sync homebody's .lein dir with git repo

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,7 @@
-{:user {:plugins [[lein-swank "1.4.0"]
-                  [lein-pprint "1.1.1"]
-                  [lein-eclipse "1.0.0"]]}}
+{:user
+ {:plugins [[lein-ritz "0.6.0"]
+            [lein-pprint "1.1.1"]
+            [lein-ring "0.7.2"]
+            [lein-clojars "0.9.1"]
+            [speclj "2.5.0"]]
+  :launch4j-install-dir "/home/geoff/bin/launch4j"}}
