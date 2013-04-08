b0e34b68ef72b0232a88ad6d4c32d6dfbe8150c9
Geoff Shannon <earthlingzephyr@gmail.com>
2013-04-08 15:43:42 -0700

    Add slamhound to lein user profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,10 @@
 {:user
+
  {:plugins [[lein-ritz "0.6.0"]
             [lein-pprint "1.1.1"]
             [lein-ring "0.7.2"]
             [lein-clojars "0.9.1"]
             [speclj "2.5.0"]]
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+  :dependencies [[slamhound "1.3.3"]]
   :launch4j-install-dir "/home/geoff/bin/launch4j"}}
