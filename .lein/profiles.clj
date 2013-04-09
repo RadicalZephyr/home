c0769cce9335e6e8670be0ee04e8d76b658f5114
Geoff Shannon <earthlingzephyr@gmail.com>
2013-04-08 17:27:17 -0700

    Add midje and marginalia, remove speclj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user
-
  {:plugins [[lein-ritz "0.6.0"]
             [lein-pprint "1.1.1"]
             [lein-ring "0.7.2"]
             [lein-clojars "0.9.1"]
-            [speclj "2.5.0"]]
+            [lein-midje "3.0.1"]
+            [lein-marginalia "0.7.1"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[slamhound "1.3.3"]]
   :launch4j-install-dir "/home/geoff/bin/launch4j"}}
