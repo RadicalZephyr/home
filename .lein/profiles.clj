3e0cfaf9737795d28f764c8cc71e435dfeed8959
Geoff Shannon <geoffpshannon@gmail.com>
2014-08-03 23:34:31 -0700

    Add cider-nrepl to lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,7 +4,8 @@
             [lein-ring "0.7.2"]
             [lein-clojars "0.9.1"]
             [lein-midje "3.0.1"]
-            [lein-marginalia "0.7.1"]]
+            [lein-marginalia "0.7.1"]
+            [cider/cider-nrepl "0.6.0"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[slamhound "1.3.3"]]
   :launch4j-install-dir "/home/geoff/bin/launch4j"}}
