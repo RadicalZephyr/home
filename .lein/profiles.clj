ad6b9e2109c6a7e7c16b2f222d9fdef41215ce00
Geoff Shannon <geoffpshannon@gmail.com>
2014-09-19 19:30:44 -0700

    Update to latest cider-nrepl version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,7 +5,7 @@
             [lein-clojars "0.9.1"]
             [lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]
-            [cider/cider-nrepl "0.6.0"]]
+            [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[slamhound "1.3.3"]]
   :launch4j-install-dir "/home/geoff/bin/launch4j"}}
