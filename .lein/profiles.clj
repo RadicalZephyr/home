a0a4664b25f85f4f4f0308369d625ab4683102f1
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-04 15:05:48 -0500

    Update to newest refactor-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,7 +5,7 @@
             [lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "0.2.2"]]
+            [refactor-nrepl "1.0.1"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                  [slamhound "1.5.5"]]}}
