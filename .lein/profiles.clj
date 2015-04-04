2cae762c25b12dce78e68507bffb5719d0827e5b
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-04 14:53:55 -0500

    Remove old version of lein-ring

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user
  {:plugins [[lein-ritz "0.6.0"]
             [lein-pprint "1.1.1"]
-            [lein-ring "0.7.2"]
             [lein-clojars "0.9.1"]
             [lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]
