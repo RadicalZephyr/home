1fa8aca6a6031a20c35780e466f5549a529132b7
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-23 03:04:08 -0500

    Update refactor-nrepl dependency

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,7 +5,7 @@
             [lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "1.0.1"]]
+            [refactor-nrepl "1.0.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[radicalzephyr/repl-utils "0.2.0"]
                  [org.clojure/tools.namespace "0.2.10"]
