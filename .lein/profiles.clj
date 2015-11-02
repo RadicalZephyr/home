87bfab4174eec4bf236a93b5ff696b01c0a8f0f6
Geoff Shannon <geoffpshannon@gmail.com>
2015-11-01 18:48:04 -0600

    Update all plugins in my lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,17 +1,15 @@
 {:user
- {:plugins [[lein-ritz "0.7.0"]
-            [lein-pprint "1.1.1"]
-            [lein-clojars "0.9.1"]
-            [lein-midje "3.1.3"]
-            [lein-checkall "0.1.1"]
-            [lein-marginalia "0.7.1"]
+ {:exclusions [cider/cider-nrepl]
+  :plugins [[lein-pprint "1.1.1"]
+            [lein-midje "3.2"]
+            [lein-marginalia "0.8.0"]
             [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [refactor-nrepl "1.1.0"]]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[radicalzephyr/repl-utils "0.2.0"]
-                 [org.clojure/tools.namespace "0.2.10"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [com.gfredericks/debug-repl "0.0.6"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [com.gfredericks/debug-repl "0.0.7"]
                  [slamhound "1.5.5"]]
   :repl-options {:init (require '[radicalzephyr.repl-utils
                                   :refer :all])
