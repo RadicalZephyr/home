efd04332ee7d97b8d913e43873b742f2f0eaa574
Geoff Shannon <geoffpshannon@gmail.com>
2016-11-29 17:27:30 -0600

    Update lein user profile dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user
  {:exclusions [cider/cider-nrepl]
-  :plugins [[lein-pprint "1.1.1"]
-            [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-pprint "1.1.2"]
+            [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [[radicalzephyr/repl-utils "0.2.0"]
                  [org.clojure/tools.trace "0.7.9"]
-                 [com.gfredericks/debug-repl "0.0.7"]
+                 [com.gfredericks/debug-repl "0.0.8"]
                  [slamhound "1.5.5"]]
   :repl-options {:init (do
                          (require '[radicalzephyr.repl-utils
