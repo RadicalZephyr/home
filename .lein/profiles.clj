38fbc842d26d7a7beb95e8f7c55cf806d9d88ebb
Zefira Shannon <earthlingzephyr@gmail.com>
2024-12-04 23:43:32 -0800

    Remove lein-ancient from global profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user
  {:exclusions [cider/cider-nrepl]
-  :plugins [[lein-pprint "1.3.2"]
-            [lein-ancient "1.0.0-RC4-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-pprint "1.3.2"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :dependencies [;;[radicalzephyr/repl-utils "0.2.0" :exclusions [org.clojure/clojure]]
                  ;; [org.clojure/tools.trace "0.7.9"]
